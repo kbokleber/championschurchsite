8d8343e94d769d95bf022eac5ab2eb2614ffb9c2
Kleber Bueno <kbokleber@yahoo.com.br>
2026-02-04 11:00:38 -0300

    Fix: criar script de entrada separado para o Dockerfile

---
+++ frontend/docker-entrypoint-custom.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+# Substituir variável BACKEND_URL no template do Nginx
+if [ -f /etc/nginx/templates/default.conf.template ]; then
+    echo "Substituindo BACKEND_URL: ${BACKEND_URL:-http://backend:8000}"
+    envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+else
+    echo "Template não encontrado, usando configuração padrão"
+fi
+
+# Executar entrypoint padrão do Nginx
+exec /docker-entrypoint.sh nginx -g "daemon off;"

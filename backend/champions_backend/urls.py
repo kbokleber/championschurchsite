2fd5e20718d59ef529c236a11f6167a623b48a62
Kleber Bueno <kbokleber@yahoo.com.br>
2026-02-04 16:54:05 -0300

    fix: rota explicita para /media/ servir arquivos de /app/media

--- backend/champions_backend/urls.py
+++ backend/champions_backend/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as static_serve
 
 from .spa_views import ServeSPAView, serve_frontend_asset, get_frontend_root
 
@@ -14,8 +15,10 @@ urlpatterns = [
     path('api/', include('eventos.urls')),
 ]
 
-# Servir arquivos de mídia (dev e prod quando backend é o único app)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir arquivos de mídia em /media/ (arquivos em /app/media no container)
+urlpatterns += [
+    path('media/<path:path>', static_serve, {'document_root': settings.MEDIA_ROOT}),
+]
 if settings.DEBUG and settings.STATICFILES_DIRS:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
